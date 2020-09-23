248abd68a29bb9e6420b4b09f6293c9b53fb4b62
Tim Choate <Tim.Choate@gryphontechnologies.com>
2020-09-23 17:17:16 -0400

    Add skeletal public website project.

---
+++ Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace www.professionaltranslator.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
