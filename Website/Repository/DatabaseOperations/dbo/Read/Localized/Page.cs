8e2bcaf0a7391e142367b25b2338c7937e44485c
Tim Choate <Tim.Choate@gryphontechnologies.com>
2020-09-23 13:02:08 -0400

    DB call for localized page.

--- Website/Repository/DatabaseOperations/dbo/Read/Localized/Page.cs
+++ Website/Repository/DatabaseOperations/dbo/Read/Localized/Page.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Object = professionaltranslator.net.Repository.Conversions.Object;
 
 namespace professionaltranslator.net.Repository.DatabaseOperations.dbo.Read.Localized
 {
     internal class Page : Base
     {
+        internal static Models.Localized.Page Item(string name, string culture)
+        {
+            using var cmd = new SqlCommand("[dbo].[GetLocalizedPage]", new Base().SqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@Enum", SqlDbType.NVarChar, 20).Value = name;
+            cmd.Parameters.Add("@Culture", SqlDbType.NVarChar, 3).Value = name;
+            using var sda = new SqlDataAdapter(cmd);
+            return Object.GetItem<Models.Localized.Page>(sda);
+        }
     }
 }
---
+++ Website/Repository/Page.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using dbLocalizedRead = professionaltranslator.net.Repository.DatabaseOperations.dbo.Read.Localized.Page;
+
+namespace professionaltranslator.net.Repository
+{
+    internal class Page
+    {
+        internal static Models.Localized.Page LocalizedItem(string name, string culture)
+        {
+            try
+            {
+                return dbLocalizedRead.Item(name, culture);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
