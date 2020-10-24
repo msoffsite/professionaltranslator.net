cfbc494151f819096f0b65f126880850654816fa
Tim Choate <tim@offsitedeveloper.com>
2020-10-24 04:45:42 -0400

    Removed website project, which will be in React.js and added project for web api.

--- Repository/DatabaseOperations/ConnectionStrings.cs
+++ Repository/DatabaseOperations/ConnectionStrings.cs
@@ -7,7 +7,8 @@ namespace professionaltranslator.net.Repository.DatabaseOperations
 {
     internal class ConnectionStrings
     {
-        internal string SqlServer => "Server=DESKTOP-CP3USJ6\\SQL2K8R2;Database=professionaltranslator.net;User Id=cg_webuser;Password=$2020$Sept;MultipleActiveResultSets=true";
+        //internal string SqlServer => "Server=DESKTOP-CP3USJ6\\SQL2K8R2;Database=professionaltranslator.net;User Id=cg_webuser;Password=$2020$Sept;MultipleActiveResultSets=true";
+        internal string SqlServer => "Server=MYSTICA-PC\\SS2K8R2;Database=professionaltranslator.net;User Id=cg_webuser;Password=$2020$Sept;MultipleActiveResultSets=true";
 
         internal ConnectionStrings() {}
     }
--- Services/Program.cs
+++ Services/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace www.professionaltranslator.net
+namespace services.professionaltranslator.net
 {
     public class Program
     {
