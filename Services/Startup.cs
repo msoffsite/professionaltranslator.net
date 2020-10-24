1d37350a810f7673c88d17c858b3c83a3f3ba210
Tim Choate <tim@offsitedeveloper.com>
2020-10-24 05:30:25 -0400

    Added save for localized testimonials.

--- Services/Startup.cs
+++ Services/Startup.cs
@@ -25,6 +25,17 @@ namespace services.professionaltranslator.net
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             services.AddControllers();
         }
 
