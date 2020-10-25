62d19262ef81f59963fcb265d15850f51352a69c
Tim Choate <tim@offsitedeveloper.com>
2020-10-25 16:01:49 -0400

    Add ignored files to source control.

---
+++ repository.professionaltranslator.net/DatabaseOperations/Log/Read/Exception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Object = Repository.ProfessionalTranslator.Net.Conversions.Object;
+
+namespace Repository.ProfessionalTranslator.Net.DatabaseOperations.Log.Read
+{
+    internal class Exception : Base
+    {
+        internal static async Task<Tables.Log.Exception> Item(Guid? id)
+        {
+            await using var cmd = new SqlCommand("[Log].[GetException]", new Base().SqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@Id", SqlDbType.UniqueIdentifier).Value = id;
+            using var sda = new SqlDataAdapter(cmd);
+            return Object.GetItem<Tables.Log.Exception>(sda);
+        }
+
+        internal static async Task<List<Tables.Log.Exception>> List(string site)
+        {
+            await using var cmd = new SqlCommand("[Log].[GetExceptions]", new Base().SqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.Add("@Site", SqlDbType.NVarChar, 25).Value = site;
+            using var sda = new SqlDataAdapter(cmd);
+            return Object.GetList<Tables.Log.Exception>(sda);
+        }
+    }
+}
---
+++ repository.professionaltranslator.net/DatabaseOperations/Log/Write/Exception.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Repository.Professionaltranslator.Net;
+
+namespace Repository.ProfessionalTranslator.Net.DatabaseOperations.Log.Write
+{
+    internal class Exception : Base
+    {
+        internal static async Task<Result> Item(string site, Tables.Log.Exception item)
+        {
+            SaveStatus saveStatus;
+            var messages = new List<string>();
+
+            try
+            {
+                await using var cmd = new SqlCommand("[Log].[SaveException]", new Base().SqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.Add("@Site", SqlDbType.NVarChar, 20).Value = site;
+                cmd.Parameters.Add("@Id", SqlDbType.UniqueIdentifier).Value = item.Id;
+                cmd.Parameters.Add("@Message", SqlDbType.NVarChar, -1).Value = item.Message;
+                cmd.Parameters.Add("@Stacktrace", SqlDbType.NVarChar, -1).Value = item.Stacktrace;
+                cmd.Parameters.Add("@Type", SqlDbType.NVarChar, 50).Value = item.Type;
+                cmd.Parameters.Add("@Class", SqlDbType.NVarChar, 50).Value = item.Class;
+                await cmd.Connection.OpenAsync();
+                await cmd.ExecuteNonQueryAsync();
+                await cmd.Connection.CloseAsync();
+                saveStatus = SaveStatus.Succeeded;
+            }
+            catch (System.Exception ex)
+            {
+                saveStatus = SaveStatus.Failed;
+                messages.Add(ex.Message);
+            }
+            return new Result(saveStatus, messages);
+        }
+    }
+}
