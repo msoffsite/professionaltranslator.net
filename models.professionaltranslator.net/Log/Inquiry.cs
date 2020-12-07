775b8bc1f338ee8339f8dfb98b727ea583280289
Tim Choate <tim@offsitedeveloper.com>
2020-12-07 10:29:44 -0500

    Added missed file.

---
+++ models.professionaltranslator.net/Log/Inquiry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.ProfessionalTranslator.Net.Log
+{
+    public class Inquiry : Base
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Title { get; set; }
+        public string TranslationType { get; set; }
+        public string Genre { get; set; }
+        public int WordCount { get; set; }
+        public string Message { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
