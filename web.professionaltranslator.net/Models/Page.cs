6eba5b4cbc09044c3983af55a3d621b13d72d51e
Tim Choate <tim@offsitedeveloper.com>
2020-12-09 10:19:56 -0500

    Layout adjustment. Page model update.

--- web.professionaltranslator.net/Models/Page.cs
+++ web.professionaltranslator.net/Models/Page.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Repository.ProfessionalTranslator.Net;
 using Base = Models.ProfessionalTranslator.Net.Page;
 
 namespace web.professionaltranslator.net.Models
--- web.professionaltranslator.net/Pages/Base.cs
+++ web.professionaltranslator.net/Pages/Base.cs
@@ -20,11 +20,11 @@ namespace web.professionaltranslator.net.Pages
 
             basePage.Bodies = basePage.Bodies.Where(x => x.Lcid == configuration.Lcid).ToList();
             basePage.Headers = basePage.Headers.Where(x => x.Lcid == configuration.Lcid).ToList();
-
+            
             output.Body = basePage.Bodies.Any() ? basePage.Bodies[0].Html : "Missing Body";
             output.Title = basePage.Bodies.Any() ? basePage.Bodies[0].Title : "Missing Title";
             output.Header = basePage.Headers.Any() ? basePage.Headers[0].Html : "Missing Header";
-
+            output.Image = basePage.Image;
             return output;
         }
     }
