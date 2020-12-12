9dede89a4737ba7a43dda794d4b20ae8426062a1
Tim Choate <tim@offsitedeveloper.com>
2020-12-12 13:15:31 -0500

    Initial footer complete.

--- web.professionaltranslator.net/wwwroot/js/global.js
+++ web.professionaltranslator.net/wwwroot/js/global.js
@@ -5,3 +5,35 @@
 $(window).on("beforeunload", function () {
     $(".loading-container").fadeIn("slow");
 });
+
+
+function scroll_to(clicked_link, nav_height) {
+    const element_class = clicked_link.attr("href");
+    console.log(element_class);
+    let scroll_to = 0;
+    if (element_class != "#wrapper") {
+        //element_class += "-container";
+        scroll_to = $(element_class).offset().top - nav_height;
+    }
+    if ($(window).scrollTop() != scroll_to) {
+        $("html, body").stop().animate({ scrollTop: scroll_to }, 750);
+    }
+}
+
+
+$(document).ready(function () {
+
+    /*
+        Scroll link
+    */
+    $("a.scroll-link").on("click", function (e) {
+        e.preventDefault();
+        scroll_to($(this), 0);
+    });
+
+    /*
+        Wow
+    */
+    new WOW().init();
+
+});
