f5d4705f20e52d4d823525721f9f5a33579723ab
Tim Choate <tim@offsitedeveloper.com>
2021-04-06 11:08:32 -0400

    Layout enhancements.

--- web.professionaltranslator.net/wwwroot/js/layout.js
+++ web.professionaltranslator.net/wwwroot/js/layout.js
@@ -1,4 +1,17 @@
-﻿$(document).ready(function () {
+﻿$.fn.toggleGetStarted = function () {
+    $(".info-safe-with-me").hide();
+
+    const path = $(location).attr("pathname").toLowerCase();
+    const idx = path.toString().indexOf("contact");
+    if (idx > -1) {
+        $(".get-started").hide();
+        $(".info-safe-with-me").show();
+    }
+};
+
+$(document).ready(function () {
+
+    $(".get-started").toggleGetStarted();
 
     $(window).on("load resize", function () {
         //console.log("sidebar toggle.");
