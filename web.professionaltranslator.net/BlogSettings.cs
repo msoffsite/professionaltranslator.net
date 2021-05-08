17f5123f501265574005c4e4060e5b980bf21cbe
Tim Choate <tim@offsitedeveloper.com>
2021-05-08 14:16:10 -0400

    Support files added.

---
+++ web.professionaltranslator.net/BlogSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.professionaltranslator.net
+{
+    public class BlogSettings
+    {
+        public int CommentsCloseAfterDays { get; set; } = 10;
+
+        public bool DisplayComments { get; set; } = true;
+
+        public PostListView ListView { get; set; } = PostListView.TitlesAndExcerpts;
+
+        public string Owner { get; set; } = "The Owner";
+
+        public int PostsPerPage { get; set; } = 4;
+    }
+}
---
+++ web.professionaltranslator.net/Services/IBlogService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web.professionaltranslator.net.Models;
+
+namespace web.professionaltranslator.net.Services
+{
+    public interface IBlogService
+    {
+        Task DeletePost(Post post);
+
+        IAsyncEnumerable<string> GetCategories();
+
+        Task<Post?> GetPostById(string id);
+
+        Task<Post?> GetPostBySlug(string slug);
+
+        IAsyncEnumerable<Post> GetPosts();
+
+        IAsyncEnumerable<Post> GetPosts(int count, int skip = 0);
+
+        IAsyncEnumerable<Post> GetPostsByCategory(string category);
+
+        Task<string> SaveFile(byte[] bytes, string fileName, string? suffix = null);
+
+        Task SavePost(Post post);
+    }
+}
