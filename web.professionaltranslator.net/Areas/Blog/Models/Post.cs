6113941ab78aa3f909c3d29c45183e85f794b304
Tim Choate <tim@offsitedeveloper.com>
2021-05-17 00:31:15 -0400

    Code improvement.

--- web.professionaltranslator.net/Areas/Blog/Models/Post.cs
+++ web.professionaltranslator.net/Areas/Blog/Models/Post.cs
@@ -14,13 +14,10 @@ namespace web.professionaltranslator.net.Areas.Blog.Models
 
         public IList<Comment> Comments { get; } = new List<Comment>();
 
-        [Required]
         public string Content { get; set; } = string.Empty;
 
-        [Required]
         public string Excerpt { get; set; } = string.Empty;
 
-        [Required]
         public string Id { get; set; } = DateTime.UtcNow.Ticks.ToString(CultureInfo.InvariantCulture);
 
         public bool IsPublished { get; set; } = true;
@@ -31,7 +28,6 @@ namespace web.professionaltranslator.net.Areas.Blog.Models
 
         public string Slug { get; set; } = string.Empty;
 
-        [Required]
         public string Title { get; set; } = string.Empty;
 
         public static string CreateSlug(string title)
@@ -55,38 +51,34 @@ namespace web.professionaltranslator.net.Areas.Blog.Models
 
         public string RenderContent()
         {
-            var result = Content;
+            string result = Content;
 
             // Set up lazy loading of images/iframes
-            if (!string.IsNullOrEmpty(result))
-            {
-                // Set up lazy loading of images/iframes
-                //var replacement = " src=\"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==\" data-src=\"";
-                //var pattern = "(<img.*?)(src=[\\\"|'])(?<src>.*?)([\\\"|'].*?[/]?>)";
-                //result = Regex.Replace(result, pattern, m => m.Groups[1].Value + replacement + m.Groups[4].Value + m.Groups[3].Value);
-
-                // Youtube content embedded using this syntax: [youtube:xyzAbc123]
-                var video = "<div class=\"video\"><iframe width=\"560\" height=\"315\" title=\"YouTube embed\" src=\"about:blank\" data-src=\"https://www.youtube-nocookie.com/embed/{0}?modestbranding=1&amp;hd=1&amp;rel=0&amp;theme=light\" allowfullscreen></iframe></div>";
-                result = Regex.Replace(
-                    result,
-                    @"\[youtube:(.*?)\]",
-                    m => string.Format(CultureInfo.InvariantCulture, video, m.Groups[1].Value));
-            }
+            if (string.IsNullOrEmpty(result)) return result;
+            
+            // Youtube content embedded using this syntax: [youtube:xyzAbc123]
+            const string video = "<div class=\"video\"><iframe width=\"560\" height=\"315\" title=\"YouTube embed\" src=\"about:blank\" data-src=\"https://www.youtube-nocookie.com/embed/{0}?modestbranding=1&amp;hd=1&amp;rel=0&amp;theme=light\" allowfullscreen></iframe></div>";
+            
+            result = Regex.Replace(
+                result,
+                @"\[youtube:(.*?)\]",
+                m => string.Format(CultureInfo.InvariantCulture, video, m.Groups[1].Value));
 
             return result;
         }
 
         private static string RemoveDiacritics(string text)
         {
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            string normalizedString = text.Normalize(NormalizationForm.FormD);
             var stringBuilder = new StringBuilder();
 
-            foreach (var c in normalizedString)
+            // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
+            foreach (char entry in normalizedString)
             {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                UnicodeCategory unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(entry);
                 if (unicodeCategory != UnicodeCategory.NonSpacingMark)
                 {
-                    stringBuilder.Append(c);
+                    stringBuilder.Append(entry);
                 }
             }
 
@@ -97,9 +89,10 @@ namespace web.professionaltranslator.net.Areas.Blog.Models
         {
             var reservedCharacters = new List<string> { "!", "#", "$", "&", "'", "(", ")", "*", ",", "/", ":", ";", "=", "?", "@", "[", "]", "\"", "%", ".", "<", ">", "\\", "^", "_", "'", "{", "}", "|", "~", "`", "+" };
 
-            foreach (var chr in reservedCharacters)
+            // ReSharper disable once ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
+            foreach (string reservedCharacter in reservedCharacters)
             {
-                text = text.Replace(chr, string.Empty, StringComparison.OrdinalIgnoreCase);
+                text = text.Replace(reservedCharacter, string.Empty, StringComparison.OrdinalIgnoreCase);
             }
 
             return text;
