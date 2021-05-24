48209dfd19d7860ca43091370f64812b6f93bb80
Tim Choate <tim@offsitedeveloper.com>
2021-05-24 05:37:48 -0400

    User unsubscribe complete.

--- web.professionaltranslator.net/Pages/Unsubscribe.cshtml.cs
+++ web.professionaltranslator.net/Pages/Unsubscribe.cshtml.cs
@@ -1,16 +1,61 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Repository.ProfessionalTranslator.Net;
+using web.professionaltranslator.net.Extensions;
+using web.professionaltranslator.net.Models;
+
+using SubscriberModel = Models.ProfessionalTranslator.Net.Subscriber;
+using Data = Repository.ProfessionalTranslator.Net.Subscriber;
+
+using Exception = System.Exception;
 
 namespace web.professionaltranslator.net.Pages
 {
-    public class UnsubscribeModel : PageModel
+    public class UnsubscribeModel : Base
     {
-        public void OnGet()
+        public UnsubscribeModel(SiteSettings siteSettings)
         {
+            SiteSettings = siteSettings;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Item = await new Base().Get(SiteSettings, Area.Root, "Unsubscribe");
+            return Item == null ? NotFound() : (IActionResult)Page();
+        }
+
+        public async Task<ActionResult> OnPostSend(string emailAddress)
+        {
+            Result result;
+            try
+            {
+                SubscriberModel subscriber = await Data.Item(SiteSettings.Site, Area.Blog, emailAddress);
+                if (subscriber == null)
+                {
+                    throw new NullReferenceException($"No subscriber found for '{emailAddress}.");
+                }
+
+                result = await Data.Delete(SiteSettings.Site, subscriber.Id);
+                if (result.Status == ResultStatus.Failed)
+                {
+                    return new JsonResult(result);
+                }
+                result = new Result(ResultStatus.Succeeded, $"{emailAddress} has been unsubscribed.", Guid.Empty);
+            }
+            catch (Exception ex)
+            {
+                result = new Result(ResultStatus.Failed, ex.Message, Guid.Empty);
+            }
+
+            return new JsonResult(result);
         }
     }
 }
---
+++ web.professionaltranslator.net/wwwroot/js/unsubscribe.js
@@ -0,0 +1,51 @@
+﻿const unsubscribeResultRow = $("#unsubscribe_result_row");
+
+const unsubscribeResultText = $("#unsubscribe_result_text");
+
+let validEmailAddress = false;
+
+$(document).ready(function () {
+    "use strict";
+
+    $("#unsubscribe_me").on("click", function (e) {
+        e.preventDefault();
+        sendMessage();
+    });
+});
+
+function sendMessage() {
+
+    const input = $(".validate-input .input-element");
+
+    let passed = true;
+
+    for (let i = 0; i < input.length; i++) {
+        if (validate(input[i]) === false) {
+            showValidationMessage(input[i]);
+            passed = false;
+        }
+    }
+
+    if (passed) {
+        $.ajax({
+            type: "POST",
+            url: `/Unsubscribe/?handler=Send`,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: {
+                emailAddress: $("#unsubscribe_email_address").val()
+            },
+            success: function (response) {
+                processResultMessages(response.status, unsubscribeResultRow, unsubscribeResultText, response.messages);
+                if (response.status > 0 ) {
+                    $("#unsubscribe_email_address").val("");
+                }
+            },
+            failure: function (xhr) {
+                processResultMessages(0, unsubscribeResultRow, unsubscribeResultText, xhr.statusText);
+            }
+        });
+    }
+}
